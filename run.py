038f8b6feb13a2ee6d0abd043c78c791f6c5aa40
Muhammad Moiz Rashad <moizmister786@gmail.com>
2026-04-05 15:23:54 +0500

    asd

--- run.py
+++ run.py
@@ -1765,4 +1765,5 @@ def delete_employee(emp_id):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
